8feb766ed6fbfc5df7e85f79f062e8e08d425a9f
Gerry Sihaj <gsihaj5@gmail.com>
2022-10-27 11:03:14 +0700

    feat: api

---
+++ app/src/main/java/com/example/contact/ContactApiAdapter.java
@@ -0,0 +1,77 @@
+package com.example.contact;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+
+public class ContactApiAdapter extends ArrayAdapter<Contact> {
+    Database database;
+
+    public ContactApiAdapter(Context context, ArrayList<Contact> contacts) {
+        super(context, 0, contacts);
+        database = new Database(context);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextInputEditText name;
+        private final TextInputEditText number;
+
+        private final ImageButton buttonDelete;
+        private final ImageButton buttonUpdate;
+
+        public ViewHolder(View view) {
+            super(view);
+
+            name = view.findViewById(R.id.name);
+            number = view.findViewById(R.id.number);
+
+            buttonDelete = view.findViewById(R.id.delete);
+            buttonUpdate = view.findViewById(R.id.update);
+        }
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Contact contact = getItem(position);
+
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.contact_list, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.name.setText(contact.getName());
+        viewHolder.number.setText(contact.getNumber());
+
+        viewHolder.buttonDelete.setOnClickListener(view -> {
+            remove(contact);
+            database.deleteContact(contact.getName());
+        });
+
+        viewHolder.buttonUpdate.setOnClickListener(view -> {
+            database.updateContact(contact.getName(),
+                    new Contact(
+                            viewHolder.name.getText().toString(),
+                            viewHolder.number.getText().toString()
+                    ));
+            Toast.makeText(view.getContext(), contact.getName() + " updated to " + viewHolder.name.getText().toString(), Toast.LENGTH_LONG).show();
+        });
+
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/contact/FirstFragment.java
+++ app/src/main/java/com/example/contact/FirstFragment.java
@@ -3,6 +3,7 @@ package com.example.contact;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,7 +11,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,7 +24,12 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.example.contact.databinding.FragmentFirstBinding;
 import com.google.android.material.textfield.TextInputEditText;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class FirstFragment extends Fragment {
 
@@ -29,6 +37,7 @@ public class FirstFragment extends Fragment {
     private Database db;
     private ContactAdapter adapter;
     private LayoutInflater inflater;
+    private ArrayList<HashMap<String, String>> list_contact;
 
     @Override
     public View onCreateView(
@@ -38,6 +47,7 @@ public class FirstFragment extends Fragment {
 
         this.inflater = inflater;
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+        list_contact = new ArrayList<>();
         return binding.getRoot();
 
     }
@@ -48,7 +58,8 @@ public class FirstFragment extends Fragment {
 
         db = new Database(getContext());
 
-        loadAllContact();
+//        loadAllContact();
+        new GetContactService().execute();
 
         binding.search.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,7 +83,7 @@ public class FirstFragment extends Fragment {
         binding.clearSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                loadAllContact();
+//                loadAllContact();
 
             }
         });
@@ -139,6 +150,71 @@ public class FirstFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private class GetContactService extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(getContext(), "Loading JSON Data", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            HttpHandler sh = new HttpHandler();    // Making a request to url and getting response
+            String url = "https://gist.githubusercontent.com/Baalmart/8414268/raw/43b0e25711472de37319d870cb4f4b35b1ec9d26/contacts";
+            String jsonStr = sh.makeServiceCall(url);
+            Log.e("Main", "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);                // Getting JSON Array node
+                    JSONArray contacts = jsonObj.getJSONArray("contacts");                // looping through All Contacts
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+                        String id = c.getString("id");
+                        String name = c.getString("name");
+                        String email = c.getString("email");
+                        String address = c.getString("address");
+                        String gender = c.getString("gender");                // Phone node is JSON Object
+                        JSONObject phone = c.getJSONObject("phone");
+                        String mobile = phone.getString("mobile");
+                        String home = phone.getString("home");
+                        String office = phone.getString("office");                    // tmp hash map for single contact
+                        HashMap<String, String> contact = new HashMap<>();                // adding each child node to HashMap key => value
+                        contact.put("id", id);
+                        contact.put("name", name);
+                        contact.put("email", email);
+                        contact.put("mobile", mobile);                // adding contact to contact list
+                        list_contact.add(contact);
+                    }
+                } catch (final JSONException e) {
+                    Log.e("Main", "Json parsing error: " + e.getMessage());
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getContext(), "Json parsing error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            } else {
+                Log.e("Main", "Couldn't get json from server.");
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getContext(), "Couldn't get json from server. Check LogCat for possible errors!", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            ListAdapter adapter = new SimpleAdapter(getContext(), list_contact, R.layout.contact_api_list, new String[]{"id", "name", "email", "mobile"}, new int[]{R.id.id, R.id.name, R.id.email, R.id.mobile});
+            binding.contactList.setAdapter(adapter);
+        }
     }
 
 }
